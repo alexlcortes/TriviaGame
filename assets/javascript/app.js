e188996eba57d10f6bcf7fb5548d56c22eba7cd6
Alejandro Cortes <Alejandro Cortes>
2016-05-03 18:14:20 -0400

    second. added objects. added pseudo code.

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+var trivGame = {
+	q1: ['Kittens grow up to be cats.', true],
+	q2: ['All bugs have 5 legs.', false],
+	q3: ['4 + 5 = 8', false],
+	q4: ['Humans need water to survive.', true],
+	q5: ['Milk comes from cows.', true],
+	q6: ['9 - 3 = 6', true],
+	q7: ['Rocks are soft objects.', false],
+	q8: ['Italy is candy.', false],
+	q9: ['2 * 2 = 4', true],
+	q10: ['Cars can talk.', false]
+};
