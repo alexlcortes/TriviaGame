a3d1d29228176b7dc172781fb930a96cf315cc73
Alejandro Cortes <Alejandro Cortes>
2016-05-03 20:02:36 -0400

    updated timer. added questions. trigger on start.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,25 +18,35 @@ var timer = 30;
 $('.start').on('click', run);
 	
 
-		function run(){
-	    	counter = setInterval(increment, 1000);
-	    	$('.start').hide("slow");
-	    }
+	function run(){
+    	counter = setInterval(increment, 1000);
+    	$('.start').hide("slow");
+    	$('.quest1').html(trivGame.q1[0]);
+    	$('.quest2').html(trivGame.q2[0]);
+    	$('.quest3').html(trivGame.q3[0]);
+    	$('.quest4').html(trivGame.q4[0]);
+    	$('.quest5').html(trivGame.q5[0]);
+    	$('.quest6').html(trivGame.q6[0]);
+    	$('.quest7').html(trivGame.q7[0]);
+    	$('.quest8').html(trivGame.q8[0]);
+    	$('.quest9').html(trivGame.q9[0]);
+    	$('.quest10').html(trivGame.q10[0]);
+    }
 
-	    function increment(){
-		    timer--;
-			$('.show-counter').html('<h2>' + timer + '</h2>');
-			if (timer === 0){
-			stop();
-		    alert("Time's Up!")
-	      	}
-	    }
+    function increment(){
+	    timer--;
+		$('.show-counter').html('<h3>' + 'Time left: ' + timer + ' seconds' + '</h3>');
+		if (timer === 0){
+		stop();
+	    alert("Time's Up!")
+      	}
+    }
 
-	    function stop(){
-	    	clearInterval(counter);
-	    }
+    function stop(){
+    	clearInterval(counter);
+    }
 
-	});
+});
    
 
 
